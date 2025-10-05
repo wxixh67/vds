ce80e09680631fdb5420bc37aa69ce072aa69126
wxixh67 <3684312337@qq.com>
2025-10-05 21:24:31 +0800

    b

--- p/新建 文本文档..py
+++ p/新建 文本文档..py
@@ -1,7 +1,7 @@
 import os
 
 # 指定目录路径
-directory = r"C:\Users\l\Documents\GitHub\vds\p\images"
+directory = r"C:\Users\l\Documents\GitHub\vds\p\yunran"
 
 # 获取目录下所有文件名
 files = os.listdir(directory)
@@ -11,4 +11,4 @@ files = [f for f in files if os.path.isfile(os.path.join(directory, f))]
 
 # 按要求格式化输出
 for file in files:
-    print(f"{{ src: './images/{file}', category: '1' }},")
+    print(f"{{ src: './yunran/{file}', category: 'yunran' }},")
